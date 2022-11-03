84a72d7666d15bf6b483bbf8d7ba7337a1d35057
Wheelsy <s3754457@student.rmit.edu.au>
2022-11-03 12:47:11 +1100

    Login & Create Account
    - Login if user credentials exist in text file
    - Create account and write credentials to text file
    - Write a diary entry to a text file named by the date and time at which it was submitted

--- main.c
+++ main.c
@@ -1,44 +1,125 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-void mainMenu(void);
-void login(void);
+void mainMenu(char[]);
+void accountMenu();
+int login(char[], char[]);
 int validateMenuSelection(char[], int, int);
-int checkUserExists();
+int checkUserExists(void);
+void writeUserToFile(char[], char[]);
+void writeDiaryEntryToFile(char[]);
+void createEntry(void);
 
-struct user
+char *userDir = "user.txt";
+char *userName;
+
+void writeUserToFile(char name[], char pw[])
 {
-    char username[20];
-    char password[20];
-};
+    FILE *file = fopen(userDir, "w");
 
-int checkUserExists()
-{
-    FILE *ptr = fopen("C:/Users/elyh1/Desktop/GitHub/DiaryBee/user.txt", "r");
-    char ch;
-
-    if (NULL == ptr)
+    if (file == NULL)
     {
         perror("file can't be opened \n");
+        exit(0);
     }
     else
     {
-        printf("content of this file are \n");
-        int lines = 0;
+        fprintf(file, "%s%s", name, pw);
+        fclose(file);
+        printf("%s\n", "Account created :)");
+        accountMenu();
+    }
+}
 
-        while (!feof(ptr))
+void writeDiaryEntryToFile(char entry[])
+{
+    char path[] = "entries/";
+    time_t mytime;
+    mytime = time(NULL);
+    char *ct = ctime(&mytime);
+    ct[strcspn(ct, "\n")] = '\0';
+    while (ct[strcspn(ct, ":")] != NULL)
+    {
+        ct[strcspn(ct, ":")] = '-';
+    }
+    while (ct[strcspn(ct, " ")] != NULL)
+    {
+        ct[strcspn(ct, " ")] = '-';
+    }
+    strcat(path, ct);
+    strcat(path, ".txt");
+    printf("%s\n", path);
+    FILE *file2 = fopen(path, "w");
+
+    if (file2 == NULL)
+    {
+        perror("file can't be opened");
+        // exit(0);
+    }
+    else
+    {
+        fprintf(file2, "%s", entry);
+        fclose(file2);
+        mainMenu(userName);
+    }
+}
+
+int login(char name[], char pw[])
+{
+    FILE *file = fopen(userDir, "r");
+    char fileContents[2][20];
+
+    int i = 0;
+    if (file == NULL)
+    {
+        perror("file can't be opened \n");
+        //exit(0);
+    }
+    else
+    {
+        char line[100];
+        int i = 0;
+        while (fgets(line, sizeof(line), file))
         {
-            ch = fgetc(ptr);
-            if (ch == '\n')
-            {
-                lines++;
-            }
+            strcpy(fileContents[i], line);
+            i++;
         }
 
-        fclose(ptr);
+        if (strcmp(fileContents[0], name) == 0 && strcmp(fileContents[1], pw) == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
 
-        if (lines == 1)
+int checkUserExists(void)
+{
+    FILE *file = fopen(userDir, "r");
+    char ch;
+
+    if (file == NULL)
+    {
+        perror("file can't be opened \n");
+        exit(0);
+    }
+    else
+    {
+        char line[100];
+        int i = 0;
+        while (fgets(line, sizeof(line), file))
+        {
+            i++;
+        }
+
+        fclose(file);
+
+        if (i >= 1)
         {
             return 1;
         }
@@ -73,13 +154,23 @@ int validateMenuSelection(char selection[], int num, int numSelections)
     return num;
 }
 
-void mainMenu(void)
+void createEntry(void)
+{
+    char entry[100];
+    printf("%s\n", "Enter Diary Entry Below:");
+    fgets(entry, 99, stdin);
+    writeDiaryEntryToFile(entry);
+}
+
+void mainMenu(char name[])
 {
     char selection[64];
     int num = 0;
 
     while (num == 0)
     {
+        name[strcspn(name, "\n")] = 0;
+        printf("---------------Hi %s---------------\n", name);
         printf("%s\n", "Main Menu");
         printf("%s\n", "1: Read");
         printf("%s\n", "2: Write");
@@ -95,7 +186,11 @@ void mainMenu(void)
         break;
 
     case 2:
-        printf("New Diary Entry");
+        createEntry();
+        break;
+
+    case 3:
+        exit(0);
         break;
 
     default:
@@ -103,28 +198,62 @@ void mainMenu(void)
     }
 }
 
-void login(void)
+void accountMenu(void)
 {
     char selection[64];
     int num = 0;
-
+    int checkUser = checkUserExists();
     while (num == 0)
     {
-        printf("%s\n", "1: Login");
-        printf("%s\n", "2: Create Account");
+        printf("%s\n", "1: Create Account");
+        if (checkUser == 1)
+        {
+            printf("%s\n", "2: Login");
+        }
+        printf("%s\n", "0: Exit");
         fgets(selection, 63, stdin);
         num = validateMenuSelection(selection, num, 2);
+        if (checkUser == 1 && num == 1)
+        {
+            printf("%s\n", "An account already exists!");
+            num = 0;
+        }
     }
 
     switch (num)
     {
     case 1:
-        printf("%s\n", "Enter username:");
+        char name[20];
+        char pw[20];
+        printf("%s\n", "Enter new username:");
+        fgets(name, 19, stdin);
+        printf("%s\n", "Enter new password:");
+        fgets(pw, 19, stdin);
+        writeUserToFile(name, pw);
         break;
-
     case 2:
-        printf("Enter new username");
+        int checkLog = 0;
+        do
+        {
+            printf("%s\n", "Enter username:");
+            fgets(name, 19, stdin);
+            printf("%s\n", "Enter password:");
+            fgets(pw, 19, stdin);
+            checkLog = login(name, pw);
+            if (checkLog == 1)
+            {
+                userName = name;
+                mainMenu(name);
+            }
+            else
+            {
+                printf("%s\n", "Incorrect login details. Try again:");
+            }
+        } while (checkLog == 0);
         break;
+    case 0:
+        printf("Goodbye!");
+        exit(0);
     default:
         break;
     }
@@ -139,6 +268,5 @@ int main(int argc, char *argv[])
     printf("%s\n", "       (8|)_}}- .      .        .");
     printf("%s\n", "        `\\__)   '. . ' ' .  . ");
     printf("\n");
-    checkUserExists();
-    login();
+    accountMenu();
 }
